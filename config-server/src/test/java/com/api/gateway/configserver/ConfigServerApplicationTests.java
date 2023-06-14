33428b6a36a756b5e8a4fb7727dec753a1303afe
Aurelius Elvin <rvinxpadz456@gmail.com>
2023-06-14 16:50:33 +0700

    config-server

---
+++ config-server/src/test/java/com/api/gateway/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gateway.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
